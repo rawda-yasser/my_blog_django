09b8fd552595fb8428fb4dd9889aa618c1815628
Rawda Yasser <rawdayasserg@gmail.com>
2023-09-14 11:14:21 +0300

    Feat: update post url to use publish date and slug

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'PUBLISHED'
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -34,5 +34,5 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
     
