7740a1933c0fed8ecdcb3154abe5e05f34b0f2bd
Rawda Yasser <rawdayasserg@gmail.com>
2023-09-14 11:57:00 +0300

    Feat: add pagination template and include it in post list

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.core.paginator import Paginator
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, day, month, year, post):
