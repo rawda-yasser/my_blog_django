b6db90ac8d1ffe4bd264d27afecc209386c2646c
Rawda Yasser <rawdayasserg@gmail.com>
2023-09-14 13:24:18 +0300

    Feat: add share template and view

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,28 @@ from django.core.paginator import Paginator, EmptyPage,\
     PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you to read "\
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
